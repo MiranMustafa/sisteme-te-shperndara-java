33884b289fdf7c96a1deeab60e70776e6e20c6de
Miran Mustafa <miran@frakton.com>
2020-06-12 22:40:51 +0200

    Deletet unused files

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -51,12 +51,6 @@ public class Main {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
  @GetMapping("/getData")
  @ResponseBody
  HashMap<String, String> getData() {
@@ -65,37 +59,5 @@ public class Main {
     data.put("OS" , System.getProperty("os.name"));
     return data;
  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
 
 }
